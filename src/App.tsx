ad86dece12c23b65f31c9f874ff9a3921b8df080
acs237 <acs23072000@gmail.com>
2025-05-31 11:42:50 +1000

    change module hierarchy, started spinthewheel feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,13 @@
 import './App.css'
-import Introduction from './components/Introduction'
-import DegreeYear from './components/DegreeYear'
-import Why from './components/Why'
-import Experience from './components/Experience'
-import Hobbies from './components/Hobbies'
+import MyIntro from './pages/MyIntro'
 
 function App() {
 
   return (
     <div className='bg-black'>
       <div className='flex items-center justify-center w-full p-6'><h1 className='bg-gradient-to-r from-blue-500 to-green-500 bg-clip-text text-5xl font-extrabold text-transparent'>Aye Chan San</h1></div>
+      <MyIntro />
       
-      <Introduction />
-      <DegreeYear />
-      <Why />
-      <Experience />
-      <Hobbies />
     </div>
   )
 }
---
+++ src/pages/MyIntro.tsx
@@ -0,0 +1,20 @@
+import Introduction from '../components/Introduction';
+import DegreeYear from '../components/DegreeYear';
+import Why from '../components/Why';
+import Experience from '../components/Experience';
+import Hobbies from '../components/Hobbies';
+
+const MyIntro = () => {
+    return (
+        <div>
+            <Introduction />
+            <DegreeYear />
+            <Why />
+            <Experience />
+            <Hobbies />
+        </div>
+        
+    )
+}
+
+export default MyIntro;
